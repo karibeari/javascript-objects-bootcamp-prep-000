18b2b5090da053901b40acd9875cc4f7777b5b61
Kari Ferenczy <klferenczy@gmail.com>
2019-01-26 18:03:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {UB40: "Red Red Wine"};
 
 function updatePlaylist(playlist, artistName, songTitle){
+  delete playlist.UB40;
   playlist.artistName = songTitle;
   return playlist;
 }
